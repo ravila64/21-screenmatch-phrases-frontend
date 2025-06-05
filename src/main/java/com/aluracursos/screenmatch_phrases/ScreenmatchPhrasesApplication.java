5feddef288d34d7eb90809b7561ab5453b8d89ca
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-06-05 18:51:19 -0500

    first commit

---
+++ src/main/java/com/aluracursos/screenmatch_phrases/ScreenmatchPhrasesApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch_phrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchPhrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchPhrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/screenmatch_phrases/ScreenmatchPhrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch_phrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchPhrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
