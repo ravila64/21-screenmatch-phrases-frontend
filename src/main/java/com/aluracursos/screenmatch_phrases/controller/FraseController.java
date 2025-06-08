897712c2454092356d4c90094efe015b571e645e
Rene Avila <rene.avila.alonso@gmail.com>
2025-06-07 22:46:02 -0500

    add @CrossOrigin in controller

--- src/main/java/com/aluracursos/screenmatch_phrases/controller/FraseController.java
+++ src/main/java/com/aluracursos/screenmatch_phrases/controller/FraseController.java
@@ -3,10 +3,12 @@ package com.aluracursos.screenmatch_phrases.controller;
 import com.aluracursos.screenmatch_phrases.dto.FraseDTO;
 import com.aluracursos.screenmatch_phrases.service.FraseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class FraseController {
    @Autowired
    FraseService service;
