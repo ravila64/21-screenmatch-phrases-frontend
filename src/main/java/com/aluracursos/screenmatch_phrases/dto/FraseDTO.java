810e09b70bab68e4845638337e1b155615af9aba
Rene Avila <rene.avila.alonso@gmail.com>
2025-06-07 22:51:30 -0500

    ordenar campos

--- src/main/java/com/aluracursos/screenmatch_phrases/dto/FraseDTO.java
+++ src/main/java/com/aluracursos/screenmatch_phrases/dto/FraseDTO.java
@@ -2,8 +2,8 @@ package com.aluracursos.screenmatch_phrases.dto;
 
 public record FraseDTO(
       String titulo,
-      String poster,
       String frase,
+      String poster,
       String personaje
 ) {
 }
